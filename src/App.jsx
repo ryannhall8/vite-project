5786362cad7f9f0de5bfe86267d97eca4b66a875
Ryann Hall <ryannhall@Ryanns-MacBook-Air.local>
2024-01-09 20:56:02 -0500

    added css/ finshed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
 import { useState } from 'react'
-import {puppyList} from './data.js'
+import { puppyList } from './data.js'
+import './App.css'
 
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
-  
-  console.log("puppyList: ", puppyList);
+  console.log("puppyList", puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  function handleClick(puppyId) {
+    console.log("puppy id", puppyId);
+    setFeatPupId(puppyId);
+  }
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
 
   return (
     <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
+      {}
+      <div className="puppyList">
+      <h1>Puppy List</h1>
+      {puppies.map((puppy) => (
+        <p key={puppy.id} onClick={() => handleClick(puppy.id)}>
+          {puppy.name}
+        </p>
+      ))}
+</div>
+      {featPupId && (
+        <div className="featuredPupInfo">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
-
 export default App
