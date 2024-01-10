4931f124c4a2e75b8e73ac6b2c4fb19c7633d660
Ryann Hall <ryannhall@Ryanns-MacBook-Air.local>
2024-01-09 20:09:09 -0500

    puppy names rendered to the screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react'
+import {puppyList} from './data.js'
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [puppies, setPuppies] = useState(puppyList);
+  
+  console.log("puppyList: ", puppyList);
 
   return (
-    <>
-      hi Ry
-    </>
-  )
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
 
 export default App
